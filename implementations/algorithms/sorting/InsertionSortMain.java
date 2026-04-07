b9818237e8c486aa3fc2be5c9ce250e6e1f7032a
layaungphyo <layaungphyo079@gmail.com>
2026-04-07 12:16:53 +0700

    add insertion sort algorithm implementation

---
+++ implementations/algorithms/sorting/InsertionSortMain.java
@@ -0,0 +1,16 @@
+package algorithms.sorting;
+
+public class InsertionSortMain {
+    public static int[] InsertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int j = i - 1;
+            while (j >= 0 && arr[j + 1] < arr[j]) {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+        return arr;
+    }
+}
