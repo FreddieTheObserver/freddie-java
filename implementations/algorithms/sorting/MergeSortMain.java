5bafb9e6493faa083bb9ae7f22322f254a818d65
layaungphyo <layaungphyo079@gmail.com>
2026-04-07 23:26:35 +0700

    implementation for mergesort

---
+++ implementations/algorithms/sorting/MergeSortMain.java
@@ -0,0 +1,30 @@
+package algorithms.sorting;
+
+public class MergeSortMain {
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left >= right) return;
+
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int[] tmp = new int[right - left + 1];
+        int i = left, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                tmp[k++] = arr[i++];
+            } else {
+                tmp[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) tmp[k++] = arr[i++];
+        while (j <= right) tmp[k++] = arr[j++];
+
+        System.arraycopy(tmp, 0, arr, left, tmp.length);
+    }
+}
