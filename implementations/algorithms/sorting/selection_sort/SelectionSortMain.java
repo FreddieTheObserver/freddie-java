fe9b7d25d004152c63c6c7e83a97b3815eb44587
layaungphyo <layaungphyo079@gmail.com>
2026-04-25 17:17:39 +0700

    correct version of selection sort

--- implementations/algorithms/sorting/selection_sort/SelectionSortMain.java
+++ implementations/algorithms/sorting/selection_sort/SelectionSortMain.java
@@ -5,13 +5,13 @@ public class SelectionSortMain {
         for (int i = 0; i < arr.length - 1; i++) {
             int minIndex = i;
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[i]) {
+                if (arr[j] < arr[minIndex]) {
                     minIndex = j;
                 }
-                int temp = arr[i];
-                arr[i] = arr[minIndex];
-                arr[minIndex] = arr[temp];
             }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
         }
         return arr;
     }
