93bf31ff11e2e01f0f5bfbbcb7930dfefd6890b4
layaungphyo <layaungphyo079@gmail.com>
2026-04-27 23:01:21 +0700

    heap sort algo

---
+++ implementations/algorithms/sorting/heap_sort/HeapSortMain.java
@@ -0,0 +1,43 @@
+package algorithms.sorting.heap_sort;
+
+public class HeapSortMain {
+    public static int[] heapSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i, 0);
+        }
+
+        return arr;
+    }
+
+    private static void heapify(int[] arr, int size, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < size && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        if (right < size && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            heapify(arr, size, largest);
+        }
+    }
+}
